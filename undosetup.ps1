27a8492a4ae43b5b84d8df92b30328f4973f810a
cs947939 <sufam5@outlook.com>
2023-11-09 15:52:20 -0600

    Update undosetup.ps1

--- undosetup.ps1
+++ undosetup.ps1
@@ -1,1 +1,5 @@
-
+$regpath = "HKLM:\SYSTEM\Setup"
+Set-ItemProperty -Path $regpath -Name cmdline -Value ""
+Set-ItemProperty -Path $regpath -Name OOBEInProgress -Value 0
+Set-ItemProperty -Path $regpath -Name SetupSupported -Value 0
+Set-ItemProperty -Path $regpath -Name SetupPhase -Value 0
