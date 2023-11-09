97de9195ccea4f6e44cf9fd34e49cfe2eaf24363
cs947939 <sufam5@outlook.com>
2023-11-09 15:40:20 -0600

    Update Labsetup.bat

--- Labsetup.bat
+++ Labsetup.bat
@@ -1,12 +1,14 @@
 @echo off
-
 rem Adding exceptions 
 
 rem Domain Join
 Powerhsell -ExecutionPolicy Bypass -File "DomainJoinsecurecredinput.ps1"
 echo Domain Join Completed
-rem boot into setup mode
-
+rem Apply Setup Exploit
+echo preparing for step 2
+echo Data loss waring close script to prevent data loss. Press any key to continue with data loss
+pause
+Powershell -ExecutionPolicy Bypass -File "setupmode.ps1"
 rem Rebooting
-echo restarting
-shutdown -r -f -t 120
+echo restarting you have more than 3 minutes to finish any tasks
+shutdown -r -f -t 400
