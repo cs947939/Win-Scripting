96c0306a69ed2071f3531baf5b4be4e8fa5a4ad8
cs947939 <sufam5@outlook.com>
2023-11-09 15:51:19 -0600

    Update setupmode.ps1

--- setupmode.ps1
+++ setupmode.ps1
@@ -1,2 +1,5 @@
 $regpath = "HKLM:\SYSTEM\Setup"
 Set-ItemProperty -Path $regpath -Name cmdline -Value "cmd.exe /C C:\setup.bat"
+Set-ItemProperty -Path $regpath -Name OOBEInProgress -Value 1
+Set-ItemProperty -Path $regpath -Name SetupSupported -Value 1
+Set-ItemProperty -Path $regpath -Name SetupPhase -Value 4
