e6127641df1d269dfdf2a4949a951bb17a93aaef
cs947939 <sufam5@outlook.com>
2023-11-10 14:48:08 -0600

    Update labdeprovision.bat

--- labdeprovision.bat
+++ labdeprovision.bat
@@ -1,7 +1,5 @@
 rem deprovision a lab machine for a new task
 uwfmgr filter disable 
-echo rebooting into setup mode
-Powershell -ExecutionPolicy Bypass -File "Setupmode.ps1"
-shutdown -r -f -t 300
-echo if this machine is no longer needed press any key to wipe data
+echo this will wipe the lab disk on the VM.
 pause
+C:\Scripts\psexec.exe -s -i "Powershell -ExecutionPolicy Bypass -File"C:\Scripts\wipelabdrive.ps1""
