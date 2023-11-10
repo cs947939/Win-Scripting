2a01102baaa0a9e513884fec494492d884423af6
cs947939 <sufam5@outlook.com>
2023-11-10 12:23:41 -0600

    Update wipelabdrive.ps1

--- wipelabdrive.ps1
+++ wipelabdrive.ps1
@@ -5,7 +5,7 @@ $methodName = "doWipeMethod"
 $sesion = New-CimSession
 
 $params = New-Object Microsoft.Management.Infrastructure.CimMethodParametersCollection
-$param = [Microsoft.Management.Infrastructure.CimMetodParameter]::Create("param", "", "String", "In")
+$param = [Microsoft.Management.Infrastructure.CimMethodParameter]::Create("param", "", "String", "In")
 
 $instance = Get-CimInstance -Namespace $namespaceName -ClassName $className -Filter "ParentID='./Vendor/MSFT' and InstanceID='RemoteWipe'"
 $sesion.InvokeMethod($namepaceName, $instance, $methodName, $params)
