f95877cea98cca6dfca6890b5ff26983ea74291a
cs947939 <sufam5@outlook.com>
2023-11-10 13:26:11 -0600

    Update labreprovision.bat

--- labreprovision.bat
+++ labreprovision.bat
@@ -1,7 +1,7 @@
-RD C:\users\UWF-Admin /S /Q
 RD C:\users\Administrator /S /Q
 Powershell -ExecutionPolicy Bypass -File "C:\undosetup.ps1"
 net user normaluser Munchy /add
+net user UWF-Admin /delete
 net localgroup Administrators normaluser /add
 net user Administrator munhcyadmin12!
 uwfmgr servicing enable
