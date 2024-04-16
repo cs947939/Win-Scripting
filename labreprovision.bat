3ae2e084075c97f9f350fde61bf40e97646a3679
cs947939 <sufam5@outlook.com>
2024-04-16 15:20:36 -0500

    Update labreprovision.bat

--- labreprovision.bat
+++ labreprovision.bat
@@ -1,7 +1,1 @@
-RD C:\users\Administrator /S /Q
-Powershell -ExecutionPolicy Bypass -File "C:\undosetup.ps1"
-net user normaluser Munchy /add
-net user UWF-Admin /delete
-net localgroup Administrators normaluser /add
-net user Administrator munhcyadmin12!
 
