0c03c32850c41221b81d93b486aab2650dd1607e
cs947939 <sufam5@outlook.com>
2024-04-16 15:18:20 -0500

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,6 +1,8 @@
 @echo off
 net user UWF-Admin Admin@munchy /add
 net localgroup Administrators UWF-Admin /add
-net user Administrator ????4265!!!!m1
+net user Administrator ????4265!!!!
+net user Computer 1234 /add
+net localgroup Administrators Computer /add
 Powershell -ExecutionPolicy Bypass -File "C:\undosetup.ps1"
 shutdown -r -f -t 0
