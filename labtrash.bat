5bb75c6c3e68745b99b50d00a4fcf6d461f47d7f
cs947939 <sufam5@outlook.com>
2024-04-16 15:19:42 -0500

    Update labtrash.bat

--- labtrash.bat
+++ labtrash.bat
@@ -1,5 +1,5 @@
 rem deprovision a lab machine for a new task
 uwfmgr filter disable 
-echo this will wipe the lab disk on the VM. if you don't wish to wipe the VM please run the reprovision prep script.
+echo this will wipe the lab disk on the VM. if you don't wish to wipe the VM please run the unfreze  script.
 pause
 C:\Scripts\psexec.exe -s -i "Powershell -ExecutionPolicy Bypass -File"C:\Scripts\wipelabdrive.ps1""
