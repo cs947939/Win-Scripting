69a08246069704cdc495c7aa29d4d19b3abac173
cs947939 <sufam5@outlook.com>
2024-04-22 15:07:31 -0500

    Update UnfrenzeVM.bat

--- UnfrenzeVM.bat
+++ UnfrenzeVM.bat
@@ -1,6 +1,6 @@
 echo executing sandbox mode
 echo acconuts disablement. The ls account, computer account, Administarator account will BE DISBALED. 
-echo only account that is usable will be UWF-Admin. The password is: insert password here
+echo only account that is usable will be UWF-Admin. The password is: Admin@munchy
 echo press any key to acknowledge these warinings
 pause
 net user ls /active:no
