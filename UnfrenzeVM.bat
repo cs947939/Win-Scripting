a48701d293d7f1efaba38f2efd9d54ec922327c2
cs947939 <sufam5@outlook.com>
2024-04-22 13:57:37 -0500

    Update UnfrenzeVM.bat

--- UnfrenzeVM.bat
+++ UnfrenzeVM.bat
@@ -1,2 +1,7 @@
 echo executing sandbox mode
+echo acconuts disablement. The ls account, normaluser account, Administarator account will BE DISBALED. 
+echo only account that is usable will be UWF-Admin
+net user ls /active:no
+net user Administrator /active:no
+net user normaluser /active:no
 uwfmgr filter disable && uwfmgr filter restart 
